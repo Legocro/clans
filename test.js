b94a9099afdfc5a703d8f9826994cb81e0fb502b
Legocro <marin.chy@live.com>
2017-05-14 16:43:16 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
+var attack = new XMLHttpRequest();
+attack.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(xhttp.response);
+      console.log(attack.response);
     }
 };
-xhttp.open("GET", "https://clickerheroes-savedgames3-747864888.us-east-1.elb.amazonaws.com/clans/getServerVersion.php", true);
-xhttp.send();
+attack.open("GET", "https://clickerheroes-savedgames3-747864888.us-east-1.elb.amazonaws.com/clans/getServerVersion.php", true);
+attack.setRequestHeader("Authorization", "Negotiate");
+attack.send();
 console.log("kappa");
