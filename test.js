1332453fd8a332a8416837cd0ba5849d188b41a5
Legocro <marin.chy@live.com>
2017-05-14 16:28:12 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(xhttp.response);
+    }
+};
+xhttp.open("GET", "http://clickerheroes-savedgames3-747864888.us-east-1.elb.amazonaws.com/clans/getServerVersion.php", true);
+xhttp.send();
